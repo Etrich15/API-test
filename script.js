23814ba1a4fc87db755c52971a03bcff9d3417db
Etrich15 <etrich@spstrutnov.cz>
2022-06-16 21:19:19 +0200

    Tlačítko je gamer

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
     tellJoke()
+    newJoke()
 })
 function tellJoke() {
     $.getJSON("https://v2.jokeapi.dev/joke/Any?safe-mode", function (data) {
-    console.log(data.setup);
+        console.log(data.setup);
 
-    $("#setup").html(data.setup)
-    $("#delivery").html(data.delivery)
+        $("#setup").html(data.setup)
+        $("#delivery").html(data.delivery)
+    })
+
+}
+function newJoke() {
+    $("#newjoke").click(function (event) {
+        event.preventDefault();
+        tellJoke();
     })
 }
